7531c16072c5542c31d610d3ea52940c64a53ea7
huyh26111999 <40221380+huyh26111999@users.noreply.github.com>
2019-05-10 18:42:50 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,10 +5,25 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
+client.on('message', async message => {
+    if (message.content.toLowerCase === 'ping') {
+        message.channel.send('pong');
+    }
+    if (message.content.toLowerCase === 'hello'|| message.content === 'hi'|| message.content === 'xin chào'|| message.content === 'Hello'|| message.content === 'Hi') {
+      message.reply(':wavinghi:');
+    }
+    if (message.content.toLowerCase === 'cười đi em') {
+      message.channel.send('hihi cười đó');
+    }
+    if (message.content.toLowerCase === 'myon ơi' || message.content === 'Myon ơi' || message.content === 'myon à' || message.content === 'Myon à'|| message.content === 'Myon ới ời ơi'|| message.content === 'myon ới ời ơi') {
+      message.channel.send('dạ ._.');
+    }
+    if (message.content.toLowerCase === 'Rụng tờ rứng') {
+      message.channel.send('Lụm lụm kìa');
+    }
+    if (message.content.toLowerCase === 'vẫy tay nào') {
+      message.channel.send(':wavinghi:');
+    }
 });
 
 // THIS  MUST  BE  THIS  WAY
