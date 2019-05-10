5be1d1be37dd7ffb5ad3b704d6ff3f3e000fdab3
huyh26111999 <40221380+huyh26111999@users.noreply.github.com>
2019-05-10 18:57:44 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,12 +3,11 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('I am ready!');
-    client.user.setActivity("bé đang tập nói",{type: "PLAYING"});
 });
 
 client.on('message', message => {
     if (message.content === 'ping') {
-    	message.channel.send('pong');
+    	message.reply('pong');
   	}
 });
 
