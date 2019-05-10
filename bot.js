67cfdf3dba520eec156d31ce831594deb5d07e59
huyh26111999 <40221380+huyh26111999@users.noreply.github.com>
2019-05-10 18:15:23 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,38 +7,23 @@ client.on('ready', () => {
 
 
 client.on('message', async message => {
-    if (message.content === 'ping') {
+    if (message.content.toLowerCase === 'ping') {
         message.channel.send('pong');
     }
-  });
-
-client.on('message', async message => {
-  if (message.content === 'hello'|| message.content === 'hi'|| message.content === 'xin chào'|| message.content === 'Hello'|| message.content === 'Hi') {
-    message.reply('<a:wavinghi:481836075589959680>');
-  }
-});
-
-client.on('message', async message => {
-    if (message.content === 'myon ơi' || message.content === 'Myon ơi' || message.content === 'myon à' || message.content === 'Myon à'|| message.content === 'Myon ới ời ơi'|| message.content === 'myon ới ời ơi') {
-        message.channel.send('dạ ._.');
+    if (message.content.toLowerCase === 'hello'|| message.content === 'hi'|| message.content === 'xin chào'|| message.content === 'Hello'|| message.content === 'Hi') {
+      message.reply('<a:wavinghi:481836075589959680>');
     }
-  });
-
-  client.on('message', async message => {
-    if (message.content === 'cười đi em') {
-        message.channel.send('hihi cười đó');
+    if (message.content.toLowerCase === 'cười đi em') {
+      message.channel.send('hihi cười đó');
     }
-  });
-
-  client.on('message', async message => {
-    if (message.content === 'Rụng tờ rứng') {
-        message.channel.send('Lụm lụm kìa');
+    if (message.content.toLowerCase === 'myon ơi' || message.content === 'Myon ơi' || message.content === 'myon à' || message.content === 'Myon à'|| message.content === 'Myon ới ời ơi'|| message.content === 'myon ới ời ơi') {
+      message.channel.send('dạ ._.');
     }
-  });
-
-  client.on('message', async message => {
-    if (message.content === 'vẫy tay nào') {
-        message.channel.send('<a:wavinghi:481836075589959680>');
+    if (message.content.toLowerCase === 'Rụng tờ rứng') {
+      message.channel.send('Lụm lụm kìa');
+    }
+    if (message.content.toLowerCase === 'vẫy tay nào') {
+      message.channel.send('<a:wavinghi:481836075589959680>');
     }
   });
 // THIS  MUST  BE  THIS  WAY
