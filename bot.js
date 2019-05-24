1e5d50af732a7d1c7048af8bae2efa8148726b7d
huyh26111999 <40221380+huyh26111999@users.noreply.github.com>
2019-05-24 15:25:01 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,35 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
-client.on('ready', () => {
-    console.log('I am ready!');
-});
-
-client.on('message', message => {
+client.on('message', async message => {
     if (message.content === 'ping') {
-        message.reply('pong');
-      }
+        message.channel.send('pong');
+    }
+  });
+
+client.on('message', async message => {
+  if (message.content === 'hello'|| message.content === 'hi'|| message.content === 'xin chào'|| message.content === 'Hello'|| message.content === 'Hi') {
+    message.reply(':wavinghi:');
+  }
 });
 
-// THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+client.on('message', async message => {
+    if (message.content === 'myon ơi' || message.content === 'Myon ơi' || message.content === 'myon à' || message.content === 'Myon à'|| message.content === 'Myon ới ời ơi'|| message.content === 'myon ới ời ơi') {
+        message.channel.send('dạ ._.');
+    }
+  });
+
+  client.on('message', async message => {
+    if (message.content === 'cười đi em') {
+        message.channel.send('hihi cười đó');
+    }
+  });
+
+  client.on('message', async message => {
+    if (message.content === 'Rụng tờ rứng') {
+        message.channel.send('Lụm lụm kìa');
+    }
+  });
+
+  client.on('message', async message => {
+    if (message.content === 'vẫy tay nào') {
+        message.channel.send(':wavinghi:');
+    }
+  });
