0bf49e834812f3a104b05b00f74302e197d4b6c5
huyh26111999 <40221380+huyh26111999@users.noreply.github.com>
2019-05-24 15:22:08 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,30 +7,8 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
-});
-client.on('message', async message => {
-    if (message.content === 'myon ơi' || message.content === 'Myon ơi' || message.content === 'myon à' || message.content === 'Myon à'|| message.content === 'Myon ới ời ơi'|| message.content === 'myon ới ời ơi') {
-        message.channel.send('dạ ._.');
-    }
-});
-client.on('message', async message => {
-    if (message.content === 'cười đi em') {
-        message.channel.send('hihi cười đó');
-    }
-});
-
-client.on('message', async message => {
-    if (message.content === 'Rụng tờ rứng') {
-        message.channel.send('Lụm lụm kìa');
-    }
-});
-
-client.on('message', async message => {
-    if (message.content === 'vẫy tay nào') {
-        message.channel.send('<a:wavinghi:481836075589959680>');
-    }
+        message.reply('pong');
+      }
 });
 
 // THIS  MUST  BE  THIS  WAY
