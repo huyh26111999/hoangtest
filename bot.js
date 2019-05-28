ac6442cbc492f93b09e7c779f69efc7c99a46334
huyh26111999 <40221380+huyh26111999@users.noreply.github.com>
2019-05-28 11:47:14 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,11 +5,11 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
+client.on("ready",async() => {
+console.log(`${client.user.username} is online!`);
+client.user.setActivity("Bé đang tập nói", {type: "LISTENING"});
 });
+
 client.on('message', async message => {
     if (message.content === 'myon ơi' || message.content === 'Myon ơi' || message.content === 'myon à' || message.content === 'Myon à'|| message.content === 'Myon ới ời ơi'|| message.content === 'myon ới ời ơi') {
         message.channel.send('dạ ._.');
